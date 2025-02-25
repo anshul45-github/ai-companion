15eb12e70b4421216ab1a3c4325617882beac2b6
Anshul <anshulmendiratta10d@gmail.com>
2025-02-26 00:10:02 +0530

    Dashboard - Created dashboard layout and tool cards for AI features

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+
 import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
 
 import { Montserrat } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const montserrat = Montserrat({
     weight: "600",
@@ -22,41 +24,43 @@ const routes = [
     {
         label: 'Conversation',
         icon: MessageSquare,
-        href: '/dashboard',
+        href: '/conversation',
         color: 'text-violet-500'
     },
     {
         label: 'Image Generation',
         icon: ImageIcon,
-        href: '/dashboard',
+        href: '/image',
         color: 'text-pink-700'
     },
     {
         label: 'Video Generation',
         icon: VideoIcon,
-        href: '/dashboard',
+        href: '/video',
         color: 'text-orange-700'
     },
     {
         label: 'Music Generation',
         icon: Music,
-        href: '/dashboard',
+        href: '/music',
         color: 'text-emerald-500'
     },
     {
         label: 'Code Generation',
         icon: Code,
-        href: '/dashboard',
+        href: '/code',
         color: 'text-green-700'
     },
     {
         label: 'Settings',
         icon: Settings,
-        href: '/dashboard',
+        href: '/settings',
     }
 ]
 
 const Sidebar = () => {
+    const pathname = usePathname();
+
     return (
         <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
             <div className="px-3 py-2 flex-1">
@@ -70,7 +74,7 @@ const Sidebar = () => {
                 </Link>
                 <div className="space-y-1">
                     {routes.map((route, index) => (
-                        <Link key={index} href={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+                        <Link key={index} href={route.href} className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition", pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
                             <route.icon className={(cn("h-5 w-5 mr-3", route.color))} />
                             {route.label}
                         </Link>
